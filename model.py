3ee91ef21eec788e4de155cb8dceb20aca3381db
Aileen Liu <aileenliu184@gmail.com>
2019-11-01 00:02:28 -0700

    created basic html files and some routes

---
+++ model.py
@@ -0,0 +1,80 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+##########################################################################
+
+class Doctor(db.Model):
+    """Show doctor info"""
+
+    __tablename__ = "doctors"
+
+    doctor_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    fname = db.Column(db.String(30), nullable=False)
+    lname = db.Column(db.String(30), nullable=False)
+
+    def __repr__(self):
+
+            return f"""<Doctor doctor_id={self.doctor_id}"""
+
+
+class Patient(db.Model):
+    """Show patient info"""
+
+    __tablename__ = "patients"
+
+    patient_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    fname = db.Column(db.String(30), nullable=False)
+    lname = db.Column(db.String(30), nullable=False)
+
+    def __repr__(self):
+
+            return f"""<Patient patient_id={self.patient_id}"""
+
+
+class Appointment(db.Model):
+    """Show appointment info"""
+
+    __tablename__ = "appointments"
+
+    appointment_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    patient_id = db.Column(db.Integer, db.ForeignKey('patients.patient_id'), 
+                           nullable=False)
+    doctor_id = db.Column(db.Integer, db.ForeignKey('doctors.doctor_id'), 
+                           nullable=False)
+    date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    kind = db.Column(db.String, nullable=False)
+
+    patients = db.relationship("Patient", backref=db.backref("appointments", 
+                               order_by=appointment_id))
+    doctors = db.relationship("Doctor", backref=db.backref("appointments", 
+                               order_by=appointment_id))
+
+    def __repr__(self):
+
+            return f"""<Appointment appointment_id={self.appointment_id}
+                        doctor_id={self.doctor_id}
+                        patient_id={self.patient_id}"""
+
+############################################################################
+
+def connect_to_db(app, db_uri='postgresql:///doctorapp'):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PstgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+if __name__ == "__main__":
+    
+    from server import app
+    connect_to_db(app)
+    print("Connected to DB.")
+
+
+
---
+++ server.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, request, redirect
+from jinja2 import StrictUndefined
+from model import connect_to_db, db, Doctor, Patient, Appointment
+
+app = Flask(__name__)
+app.jinja_env.undefined = StrictUndefined
+
+##########################################################################
+
+@app.route('/doctors', methods=['GET'])
+def index():
+    """Get a list of doctors"""
+
+    doctors = Doctor.query.all()
+
+    return render_template('doctor_list.html', doctors=doctors)
+
+
+@app.route('/appointment', methods=['GET'])
+def appointment_form():
+    """Show appointment form"""
+
+    return render_template('appointment_form.html')
+
+
+@app.route('/appointment', methods=['POST'])
+def make_appointment():
+    """Add new appointment"""
+
+    fname = request.form['fname']
+    lname = request.form['lname']
+    time
+    kind
+
+    return redirect('/')
+
+
+@app.route('/doctors/<int:doctor_id>', methods=['GET'])
+def doctor_detail(docto_id):
+    """Show a list of appointment for a specific doctor"""
+
+    doctor = Doctor.query.get(doctor_id)
+    appointment = Appointment.query.filter_by(doctor_id=doctor_id)
+
+    return render_template('appointment_detail.html', appointment=appointment)
+
+
+#########################################################################
+
+if __name__ == "__main__":
+
+    app.debug = False
+    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+    app.jinja_env.auto_reload = app.debug
+
+    connect_to_db(app)
+
+    DebugToolbarExtension(app)
+
+    app.run(port=5000, host='0.0.0.0')
+
+
+
+
+
+
+
+
+
+
